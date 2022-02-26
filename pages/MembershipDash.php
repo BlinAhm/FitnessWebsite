93947dcf59bb9b95bf3cf71fd8028f25c042c0a9
Blin Ahmeti <ba51658@ubt-uni.net>
2022-02-26 17:12:17 +0100

    Membership add and remove functionality

--- pages/MembershipDash.php
+++ pages/MembershipDash.php
@@ -1,5 +1,14 @@
 <?php
     session_start();
+    require_once '../controllers/MembershipController.php';
+    $member = new MembershipController();
+
+    if(isset($_POST['insert'])){
+        $member->addMembership($_POST);
+    }
+    if(isset($_GET['delete'])){
+        $member->removeMembership($_GET);
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -48,11 +57,55 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
 
+                        $data = $member->readData();
+                        foreach($data as $membership): ?>
+                        <tr>
+                            <td><?php echo $membership['membership_id']; ?></td>
+                            <td><?php echo $membership['type']; ?></td>
+                            <td><?php echo $membership['price'].' €'; ?></td>
+                            <td><?php echo $membership['validity'].' months'; ?></td>
+                        </tr>
+                    <?php
+                        endforeach;
+                    ?>
                 </tbody>
             </table>
         </form>
     </div>
 
+    
+    <div id="insertForm">
+        <form class="insertForm" method="POST">
+            <span class="close">&times</span>
+            <p id="headP">Add membership:</p>
+            <div>
+                <p>Membership type:</p>
+                <input class="userInputs" type="text" name="type">
+                <p>Price:</p>
+                <input class="userInputs" type="number" name="price">
+                <p>Validity (Months):</p>
+                <input class="userInputs" type="number" name="validity">
+            </div>
+            
+            <input class="save" type="submit" name="insert" value="Save">
+        </form>
+    </div>
+
+    <div id="deleteForm">
+        <form class="deleteForm" method="GET">
+            <span class="close">&times</span>
+            <p id="headP">Remove membership:</p>
+            <div>
+                <p>Membership id:</p>
+                <input class="deleteId" type="number" name="deleteId">
+            </div>
+            
+            <input class="delete" id="deleteBtn" type="submit" name="delete" value="Delete">
+        </form>
+    </div>
+
+    <script src="../js/Admin.js"></script>
 </body>
 </html>
